60cc0b967822004c2599e2f2bb9ded92d54afd90
Ignatius <109012779+Ignatius47@users.noreply.github.com>
2024-08-16 23:07:53 +0300

    Update query_samples.py

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -11,12 +11,12 @@ from relationship_app.models import Library, Book
 
 library_name = "Michelin Library"
 library = Library.objects.get(name=library_name)
-books_in_library = library.author.all()
+books_in_library = library.books.all()
 
 for book in books_in_library:
     print(book.title)
 
-from relationship_app.models import Librarian, Book
+from relationship_app.models import Librarian, Library
 
 library_name = "Michelin Library"
 library = Library.objects.get(name=library_name)
