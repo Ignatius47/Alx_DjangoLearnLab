03cbdf4b7415e8bea11d54d7459abaed9fa8fd71
Ignatius <109012779+Ignatius47@users.noreply.github.com>
2024-08-17 22:24:34 +0300

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import list_books, LibraryDetailView, admin_view, librarian_view, member_view, add_book, edit_book, delete_book
+from .views import list_books, LibraryDetailView, admin_view, librarian_view, member_view, signup, add_book, edit_book, delete_book
 
 urlpatterns = [
     path('books/', list_books, name='book-list'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
-    path('signup/', views.register, name='register'),
+    path('signup/', signup, name='register'),  # This line should remain.
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin_view, name='admin-view'),
     path('librarian/', librarian_view, name='librarian'),
@@ -13,7 +13,6 @@ urlpatterns = [
     path('book/add/', add_book, name='add-book'),
     path('book/<int:pk>/edit/', edit_book, name='edit-book'),
     path('book/<int:pk>/delete/', delete_book, name='delete-book'),
-    path('signup/', signup, name='register')
     path('login/', LoginView.as_view(template_name='yourapp/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='yourapp/logout.html'), name='logout'),
 ]
