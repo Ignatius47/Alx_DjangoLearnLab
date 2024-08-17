d12e210ecfbba9a2603789aa741f4f4c4e08099c
Ignatius <109012779+Ignatius47@users.noreply.github.com>
2024-08-17 21:34:52 +0300

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -37,9 +37,10 @@ def signup(request):
     return render(request, 'registration/register.html', {'form': form})
 
 from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile
 
 def is_admin(user):
-    return user.userprofile.role == 'ADMIN'
+    return user.is_authenticated and user.userprofile.role == 'ADMIN'
 
 def is_librarian(user):
     return user.userprofile.role == 'LIBRARIAN'
