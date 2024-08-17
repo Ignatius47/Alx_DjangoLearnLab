15360db31c87733bc0a7502a75aae5f5a489df00
Ignatius <109012779+Ignatius47@users.noreply.github.com>
2024-08-17 22:27:01 +0300

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -15,4 +15,15 @@ urlpatterns = [
     path('book/<int:pk>/delete/', delete_book, name='delete-book'),
     path('login/', LoginView.as_view(template_name='yourapp/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='yourapp/logout.html'), name='logout'),
+    
+    path('books/', list_books, name='book-list'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
+    path('signup/', views.register, name='register'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('admin/', admin_view, name='admin'),
+    path('librarian/', librarian_view, name='librarian'),
+    path('member/', member_view, name='member'),
+    path('book/add/', add_book, name='add-book'),
+    path('book/<int:pk>/edit/', edit_book, name='edit-book'),
+    path('book/<int:pk>/delete/', delete_book, name='delete-book'),
 ]
