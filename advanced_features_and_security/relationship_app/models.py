743421496b6cf903575c137fac564022c207d7d3
Ignatius <109012779+Ignatius47@users.noreply.github.com>
2024-08-23 12:55:32 +0300

    Update models.py

--- advanced_features_and_security/relationship_app/models.py
+++ advanced_features_and_security/relationship_app/models.py
@@ -24,7 +24,7 @@ class CustomUserManager(BaseUserManager):
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True, max_length=255)
     date_of_birth = models.DateField(null=True, blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
 
     objects = CustomUserManager()
 
