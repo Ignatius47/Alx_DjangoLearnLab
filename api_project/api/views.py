ba7922cf8570db637120752a6ef0ac13f51d705b
Ignatius <109012779+Ignatius47@users.noreply.github.com>
2024-08-28 12:53:09 +0300

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,20 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework import generics
 # Create your views here.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Book
 from .serializers import BookSerializer
 
-@api_view(['GET', 'POST'])
-def book_list(request):
-    if request.method == 'GET':
-        books = Book.objects.all()
-        serializer = BookSerializer(books, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = BookSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
