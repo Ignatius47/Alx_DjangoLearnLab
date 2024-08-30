0fbef69658e36f3da3d13ce600bac4f9ca9a5afa
Ignatius47 <ignatiusx47@gmail.com>
2024-08-30 11:17:57 +0300

    Updates

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
-
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
+from . views import BookListViewSet
+from rest_framework.authtoken import views as drf_views
+
+router = DefaultRouter()
+router.register(r'books', BookListViewSet)
 
 urlpatterns = [
     path('books/<int:pk>/', views.book_list, name='book_list'),
+    path('', include(router.urls)),
+    path('api-token-auth/', drf_views.obtain_auth_token, name='api_token_auth'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,20 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import viewsets, permissions
+from .permissions import IsOwnerOrReadOnly
 
-@api_view(['GET', 'POST'])
-def book_list(request):
-    if request.method == 'GET':
-        books = Book.objects.all()
-        serializer = BookSerializer(books, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = BookSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+class BookListViewSet(viewsets.ModelViewset):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permissions = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+
