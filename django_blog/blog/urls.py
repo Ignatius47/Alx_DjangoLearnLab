45b559c4cfa0f4b243c8dd2526b6e70bd3eb096b
Ignatius <109012779+Ignatius47@users.noreply.github.com>
2024-09-13 14:50:31 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('profile/', profile, name='profile'),
 
     # Post-related views
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
     path('posts/', PostListView.as_view(), name='post-list'),  # Changed this to 'posts/'
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
