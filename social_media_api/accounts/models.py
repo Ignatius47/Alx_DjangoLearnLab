01ae1c1ee3cd0a23bc9d96334ac4aebeaaec7b45
Ignatius <109012779+Ignatius47@users.noreply.github.com>
2024-09-19 21:24:42 +0300

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    bio = models.TextField(blank=True) # Optional biography field
-    profile_picture = models.ImageField(upload_to='profile_pics', blank=True) # Optional profile picture field (can be blank)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics', blank=True)
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following') # Many-to-many relationship for following/followers, asymmetrical
     
     # String representation of the user
