6b1aea027f2ceeed96005b4279a822bd4f2159a3
Ignatius <109012779+Ignatius47@users.noreply.github.com>
2024-09-23 12:22:15 +0300

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9@h6e$y#+5+3g9zeoa=arhnnqa%mz&e(guq--o@1k4^%pd^87a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
